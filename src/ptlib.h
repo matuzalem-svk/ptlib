af40c5f10f1747fd2f6fdd06574593a52d2717ec
matuzalem-svk <matus.sabol@yahoo.com>
2026-09-07 15:52:52 +0200

    moved top level Parser to ptlib.h

--- src/ptlib.h
+++ src/ptlib.h
@@ -16,4 +16,106 @@
 
 #include "common/types.h"
 #include "common/utility.h"
-#include "parser/parser.h"
+#include "parser/iparser.h"
+#include "parser/impl/slr.h"
+#include "lexer/ilexer.h"
+#include "lexer/impl/regexlexer.h"
+
+namespace ptlib 
+{
+
+template <class ParserImplClassT = parser::SLRParser, class LexerImplClassT = lexer::RegexLexer>
+class Parser
+{
+public:
+    Parser()
+    : bParserInitialized(false)
+    {
+        static_assert(std::is_base_of<lexer::ILexerImpl, LexerImplClassT>::value, "Lexer class does not implement common ILexer interface.");
+        static_assert(std::is_base_of<parser::IParserImpl, ParserImplClassT>::value, "Parser class does not implement common IParser interface.");
+    }
+
+    Parser(const common::Grammar& inGrammar)
+    : grammar(inGrammar), bParserInitialized(false)
+    {
+        static_assert(std::is_base_of<lexer::ILexerImpl, LexerImplClassT>::value, "Lexer class does not implement common ILexer interface.");
+        static_assert(std::is_base_of<parser::IParserImpl, ParserImplClassT>::value, "Parser class does not implement common IParser interface.");
+
+        lexer.Initialize(grammar);
+
+        grammar.AugmentGrammar();
+        bParserInitialized = parser.Initialize(grammar);
+    }
+
+    bool Initialize()
+    {
+        if (bParserInitialized) return true;
+
+        bParserInitialized |= lexer.Initialize(grammar);
+        grammar.AugmentGrammar();
+        bParserInitialized |= parser.Initialize(grammar);
+
+        return bParserInitialized;
+    }
+
+    const common::Symbol* AddNonterminal(const char* name, bool isStartNonterminal = false)
+    {
+        return grammar.AddNonterminal(name, isStartNonterminal);
+    }
+
+    const common::Symbol* AddTerminal(const char* name, const char* value)
+    {
+        return grammar.AddTerminal(name, value);
+    }
+
+    void AddProduction(const common::Symbol* head, const std::vector<const common::Symbol*>& tail)
+    {
+        grammar.AddProduction(head, tail);
+    }
+
+    bool Parse(const char* input)
+    {
+        if (!bParserInitialized)
+        {
+            bool failedAgain = !Initialize();
+            if (failedAgain) 
+            {
+                ptlib_out << "PARSER INITIALIZATION FAILED" << std::endl;
+                return false;
+            }
+        }
+
+        lexer.SetInput(input);
+        parser.ResetParse();
+
+        lexer.NextToken();
+        parser::ParseStepResult stepResult;
+
+        do
+        {
+            const common::Symbol* token = lexer.GetToken();
+            stepResult = parser.StepParse(token);
+            if (stepResult.requireNextToken)
+            {
+                lexer.NextToken();
+            }
+
+            if (stepResult.actionSuccess && stepResult.parseSuccess)
+            {
+                ptlib_out << "PARSE SUCCESSFUL" << std::endl;
+                break;
+            }
+        }
+        while (stepResult.actionSuccess);
+
+        return stepResult.parseSuccess;
+    }
+
+protected:
+    common::Grammar grammar;
+    LexerImplClassT lexer;
+    ParserImplClassT parser;
+    bool bParserInitialized;
+}; // class Parser
+
+} // namespace ptlib
